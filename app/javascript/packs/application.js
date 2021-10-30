3dc9d4742e170d85358d07b87ae2492008f97663
Umang Raj <umangraj@Umangs-Air.lan>
2021-10-30 07:52:27 -0400

    Chat functionality working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,4 @@ import "@hotwired/turbo-rails"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "controllers"
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :rooms
   resources :users
   root 'rooms#index'
+  resources :rooms do
+    resources :messages
+  end
   
 
   get '/signin', to: 'sessions#new'

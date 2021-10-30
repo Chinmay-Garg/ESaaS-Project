94bcff92c5cb56db047f4e7b82feef617352e773
Sounak Ray <sounakray1997@gmail.com>
2021-10-30 14:19:36 -0500

    Added 1 scenario

---
+++ features/step_definitions/private_chat_steps.rb
@@ -0,0 +1,27 @@
+Given(/^the following users exist:$/) do
+  pending
+end
+
+And(/^I am on the RottenPotatoes home page$/) do
+  pending
+end
+
+And(/^I am logged in as "([^"]*)"$/) do |arg|
+  pending
+end
+
+Then(/^I should have options to send messages to "([^"]*)", "([^"]*)" and "([^"]*)"$/) do |arg1, arg2, arg3|
+  pending
+end
+
+When(/^I send a message "([^"]*)" to "([^"]*)"$/) do |arg1, arg2|
+  pending
+end
+
+Then(/^I should see the message "([^"]*)" on the chat with "([^"]*)"$/) do |arg1, arg2|
+  pending
+end
+
+And(/^I should not see the message "([^"]*)" on the chat with "([^"]*)"$/) do |arg1, arg2|
+  pending
+end

b48af74f47b3d3cf209249d67e629d931dc200c1
Chinmay Garg <chinmay.dragon@gmail.com>
2021-10-30 04:36:20 -0400

    push chat

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'rooms/index'
+  resources :rooms
+  resources :users
+  root 'rooms#index'
+  
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+end

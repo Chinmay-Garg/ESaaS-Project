8a63b31527d6d8e1bc01351c2794d6be18dcda6d
Umang Raj <umangraj@Umangs-Air.lan>
2021-11-01 21:14:19 -0400

    Added tests for message and room model

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should have_many(:messages) }
+    it { should have_many(:participants) }
+  end
+  describe 'valid' do
+  	subject { described_class.new(name: 'general') }
+  	it { should validate_uniqueness_of(:name) }
+  end
 end

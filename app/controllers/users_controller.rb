35a511c17b9b0687e94796830864387f921fe8f6
Harry <hsg35@miami.edu>
2021-11-02 18:57:24 -0400

    savd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,14 +16,7 @@ class UsersController < ApplicationController
     def create
       #byebug
       @user = User.new(user_params)
-      if @user.save 
-        
-        puts("OK")
-      else
-      
-        puts("OK")
-      end
-
+      @user.save
     
     end
 
